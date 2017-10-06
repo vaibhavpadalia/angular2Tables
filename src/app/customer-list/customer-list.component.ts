0e4e492bb71f6a351f72c0f1c645e605f14f7a81
Vaibhav Padalia <vaibhav.padalia@kelltontech.com>
2017-10-06 14:56:17 +0530

    Changed "" to ''

--- src/app/customer-list/customer-list.component.ts
+++ src/app/customer-list/customer-list.component.ts
@@ -6,53 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-list.component.css']
 })
 export class CustomerListComponent implements OnInit {
-  records: Array<any>;
+  records: any[];
   constructor() {
     this.records = [
       {
         id: 1,
-        lastName: "doe",
-        firstName: "john",
-        email: "john.doe@example.com",
-        isActive: "yes",
-        dateCreated: "2007-08-30 23:23:13",
-        dateUpdated: "2008-08-08 12:28:24"
+        lastName: 'doe',
+        firstName: 'john',
+        email: 'john.doe@example.com',
+        isActive: 'yes',
+        dateCreated: '2007-08-30 23:23:13',
+        dateUpdated: '2008-08-08 12:28:24'
       },
       {
         id: 2,
-        lastName: "feraro",
-        firstName: "richard",
-        email: "richardferaro@localhost.local",
-        isActive: "yes",
-        dateCreated: "2010-05-10 15:25:47",
-        dateUpdated: "2010-05-10 15:25:48"
+        lastName: 'feraro',
+        firstName: 'richard',
+        email: 'richardferaro@localhost.local',
+        isActive: 'yes',
+        dateCreated: '2010-05-10 15:25:47',
+        dateUpdated: '2010-05-10 15:25:48'
       },
       {
         id: 3,
-        lastName: "dela cruz",
-        firstName: "Juan",
-        email: "juan@localhost.com",
-        isActive: "yes",
-        dateCreated: "2010-08-17 12:13:05",
-        dateUpdated: "2010-08-17 12:13:05"
+        lastName: 'dela cruz',
+        firstName: 'Juan',
+        email: 'juan@localhost.com',
+        isActive: 'yes',
+        dateCreated: '2010-08-17 12:13:05',
+        dateUpdated: '2010-08-17 12:13:05'
       },
       {
         id: 4,
-        lastName: "dela cruzx",
-        firstName: "Juan",
-        email: "juanx@localhost.com",
-        isActive: "yes",
-        dateCreated: "2010-08-17 12:19:40",
-        dateUpdated: "2010-08-17 12:19:40"
+        lastName: 'dela cruzx',
+        firstName: 'Juan',
+        email: 'juanx@localhost.com',
+        isActive: 'yes',
+        dateCreated: '2010-08-17 12:19:40',
+        dateUpdated: '2010-08-17 12:19:40'
       },
       {
         id: 5,
-        lastName: "dela Cruz",
-        firstName: "Juan",
-        email: "sample@localhost.local",
-        isActive: "yes",
-        dateCreated: "2010-08-17 12:34:48",
-        dateUpdated: "2010-08-17 12:34:48"
+        lastName: 'dela Cruz',
+        firstName: 'Juan',
+        email: 'sample@localhost.local',
+        isActive: 'yes',
+        dateCreated: '2010-08-17 12:34:48',
+        dateUpdated: '2010-08-17 12:34:48'
       }
     ];
    }
@@ -61,22 +61,22 @@ export class CustomerListComponent implements OnInit {
   }
   sortTable(n: number) {
   let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById("table1");
+  table = document.getElementById('table1');
   switching = true;
-  dir = "asc";
+  dir = 'asc';
   while (switching) {
     switching = false;
-    rows = table.getElementsByTagName("TR");
+    rows = table.getElementsByTagName('TR');
     for (i = 1; i < (rows.length - 1); i++) {
       shouldSwitch = false;
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      if (dir === "asc") {
+      x = rows[i].getElementsByTagName('TD')[n];
+      y = rows[i + 1].getElementsByTagName('TD')[n];
+      if (dir === 'asc') {
         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
           shouldSwitch = true;
           break;
         }
-      } else if (dir === "desc") {
+      } else if (dir === 'desc') {
         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
           shouldSwitch = true;
           break;
@@ -89,8 +89,8 @@ export class CustomerListComponent implements OnInit {
       switchcount++;
     }
     else {
-      if (switchcount === 0 && dir === "asc") {
-        dir = "desc";
+      if (switchcount === 0 && dir === 'asc') {
+        dir = 'desc';
         switching = true;
       }
     }
